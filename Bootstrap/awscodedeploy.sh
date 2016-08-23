d99712134607830c63d3e3ac60d92f8fd9ba356d
mala <mala@LAPTOP-6SSO91NB>
2016-08-23 11:25:32 +0200

    bootstrap

--- Bootstrap/awscodedeploy.sh
+++ Bootstrap/awscodedeploy.sh
@@ -3,6 +3,10 @@ apt-get -y update
 apt-get -y install awscli
 apt-get -y install ruby2.0
 cd /home/ubuntu
+
+export AWS_ACCESS_KEY_ID=AKIAIUS5PJ3IR535R7AQ
+export AWS_SECRET_ACCESS_KEY=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
+
 aws s3 cp s3://aws-codedeploy-eu-west-1/latest/install . --region eu-west-1
 chmod +x ./install
 ./install auto

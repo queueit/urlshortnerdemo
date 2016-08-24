e0d0f147ee1459e2c41f4bc083f4a1a5846f095b
mala <mala@LAPTOP-6SSO91NB>
2016-08-24 13:25:37 +0200

    Healthcheck + CF

---
+++ UrlShortnerDemo/Controllers/HealthcheckController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UrlShortnerDemo.Controllers
+{
+    public class HealthcheckController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Json(new { result = "ok" });
+        }
+    }
+}

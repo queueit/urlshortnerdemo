6639ac35f7f16a4dc04409286591a9990b5172cb
Martin Larsen <mala@queue-it.net>
2016-08-26 11:22:49 +0200

    Update awscodedeploy.sh

--- Bootstrap/awscodedeploy.sh
+++ Bootstrap/awscodedeploy.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 apt-get -y update
-apt-get -y install awscli
+apt-get remove awscli -y
+apt-get install python-pip -y
 apt-get -y install ruby2.0
-cd /home/ubuntu
+pip install awscli
 
-export AWS_ACCESS_KEY_ID=AKIAIUS5PJ3IR535R7AQ
-export AWS_SECRET_ACCESS_KEY=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
+cd /home/ubuntu
 
 aws s3 cp s3://aws-codedeploy-eu-west-1/latest/install . --region eu-west-1
 chmod +x ./install
 ./install auto
-
-apt-get remove awscli -y
-apt-get install python-pip -y
-pip install awscli

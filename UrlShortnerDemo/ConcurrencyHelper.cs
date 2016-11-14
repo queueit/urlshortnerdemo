3ddb5692680f0577a4e5c8888eeb405a0df3dd07
mala <mala@LAPTOP-6SSO91NB>
2016-11-14 14:51:58 +0100

    Concurrency test

--- UrlShortnerDemo/ConcurrencyHelper.cs
+++ UrlShortnerDemo/ConcurrencyHelper.cs
@@ -8,7 +8,7 @@ namespace UrlShortnerDemo
 {
     public static class ConcurrencyHelper
     {
-        static SemaphoreSlim semaphoreSlim = new SemaphoreSlim(18, 18);
+        static SemaphoreSlim semaphoreSlim = new SemaphoreSlim(20, 20);
 
         public static async Task ExecuteAsync(Func<Task> method)
         {
